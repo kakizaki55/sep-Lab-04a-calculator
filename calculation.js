ff8b733b5b81f5bb92a7eda2ae030ed504c9ae37
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-09-30 13:44:13 -0700

    got working test for adding two numbers

---
+++ calculation.js
@@ -0,0 +1,9 @@
+
+
+export function add_two(num){
+    return num + 2;
+}
+
+export function add(num1, num2){
+    return num1 + num2;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,41 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { add_two, add } from '../calculation.js';
+
 const test = QUnit.test;
+
+
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add_two(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('add function', (expect) => {
+    const input1 = 3;
+    const input2 = 5;
+    const expected = 8;
+
+    const actual = add(input1, input2);
+    expect.equal(actual, expected);
+});
+
+test('add function', (expect) => {
+    const input1 = 4;
+    const input2 = 20;
+    const expected = 24;
+
+    const actual = add(input1, input2);
+    expect.equal(actual, expected);
+});
+
