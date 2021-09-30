5c651aa9f09b9bb84d1671034ed8aca7ce1551ea
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-09-30 14:19:03 -0700

    addition working well

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-// import functions and grab DOM elements
+import { add } from './calculation.js'; 
 
-// initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const input1 = document.getElementById('number-1');
+const input2 = document.getElementById('number-2');
+const calButton = document.getElementById('calculate');
+const answer = document.getElementById('answer');
+
+console.log(input1, input2, answer);
+
+console.log(add());
+
+
+calButton.addEventListener('click', () =>{
+    console.log('button pressed');
+    const value1 = Number(input1.value);
+    const value2 = Number(input2.value);
+
+    const results = add(value1, value2);
+
+    console.log(results);
+    return results;
+  
+});
